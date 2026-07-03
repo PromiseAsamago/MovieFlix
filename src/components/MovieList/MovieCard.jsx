bf864a5aaa4bbcbc4d0ecf4d468db580603fbb39
PromiseAsamago <asamagopromise8@gmail.com>
2026-07-03 19:44:43 +0100

    API integrated

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 import './MovieCard.css'
 
-const MovieCard = () => {
+const MovieCard = ({movie}) => {
   return (
-    <a href="" className='movieCard'>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5X8poKJ9ocso3GrsXrbwqpvft4L8nRqDzLF4ptEXChw&s=10" alt="movie poster" className='moviePoster' />
+    <a href={`https://www.themoviedb.org/movie/${movie.id}`} target="_blank" className='movieCard'>
+        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="movie poster" className='moviePoster' />
         <div className="movieDetails">
-            <h4 className="movieDetailsHeading">Movie Name</h4>
+            <h4 className="movieDetailsHeading">{movie.origicdnal_title}</h4>
             <div className="movieDateRate">
-                <div>2025</div>
+                <div>{movie.release_date}</div>
                 <div className='ratingBox'>
                     <div className="ratingIcon"></div>
-                    <div className="rating">7.0</div>
+                    <div className="rating">{movie.vote_average}</div>
                 </div>
             </div>
         </div>
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './MovieList.css'
 import MovieCard from './MovieCard'
 const MovieList = () => {
+  const [movies, setmovies] = useState([]);
+  useEffect (() => {
+    fetchMovies();
+  }, []);
+  const fetchMovies = async() => {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=094dfeaf81b265f0da0a6762aeff44fd"
+    );
+    const data = await response.json();
+    setmovies(data.results);
+  }
   return (
     <section className="movieList">
         <header className="movieListHeader">
             <h3 className="movieListHeading">POPULAR MOVIES</h3>
         </header>
-        <MovieCard/>
+        <div className="movieCards">
+          {
+            movies.map(movie=><MovieCard key={movie.id} movie={movie}/>)
+          }
+        </div>
     </section>
   )
 }
