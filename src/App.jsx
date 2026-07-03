66cf2fe80c1a4a7eea91a1b34ae40476334ee8a8
PromiseAsamago <asamagopromise8@gmail.com>
2026-07-03 12:12:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './App.css'
+
+ const App = () => {
+  return (
+    <div className='app'>
+      <div className="mainHeader">
+        <h1>MovieFlix</h1>
+      </div>
+      <main>Main content</main>
+    </div>
+  )
+}
+export default App
